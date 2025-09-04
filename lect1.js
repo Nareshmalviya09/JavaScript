58eaa63d2fd99d3f57042dd9a0a98c3de36dfe8e
Nareshmalviya09 <Nareshmalviya4321@gmail.com>
2025-09-04 10:30:32 +0530

    jS file added

---
+++ lect1.js
@@ -0,0 +1,13 @@
+console.log("naresh");
+a=45;
+console.log(a);
+
+var coursename = "javascript";
+let courseid = 212;
+const coursedpt="computer since";
+
+console.log(coursename)
+console.log(courseid)
+console.log(coursedpt)
+
+console.table([coursedpt,courseid,coursename])
---
+++ typeConverstion.js
@@ -0,0 +1,17 @@
+// vallue to String
+//  String(), value.toString()
+
+// 1. number to String
+// 2. boolean to string
+
+
+let num1=45
+console.log(typeof String(num1),num1)
+
+
+//   value to number    
+// number to (value)
+
+// 1. string to number 
+let myname="naresh"
+console.log(typeof Number(myname))
